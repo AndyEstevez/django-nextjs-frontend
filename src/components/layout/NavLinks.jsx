689c7e5c353306bea1037a433715226b7654e148
andy <andyestevez27@gmail.com>
2024-09-19 11:06:27 -0400

    Added navbar navigation & links

---
+++ src/components/layout/NavLinks.jsx
@@ -0,0 +1,16 @@
+
+
+const NavLinks = [
+    {
+        label: "Dashboard",
+        authRequired: false,
+        href: "/",
+    },
+    {
+        label: "Waitlist",
+        authRequired: true,
+        href: "/",
+    }
+]
+
+export default NavLinks
