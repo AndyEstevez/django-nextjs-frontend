6311c120c3d784bb7bdc7a899604212eefcc6042
andy <andyestevez27@gmail.com>
2024-09-20 10:48:37 -0400

    Added apiproxy class

--- src/app/api/waitlists/route.jsx
+++ src/app/api/waitlists/route.jsx
@@ -1,53 +1,25 @@
 import { NextResponse } from "next/server";
 import { getToken } from "../../../lib/auth";
+import ApiProxy from "../proxy";
 
 const DJANGO_API_WAITLISTS_URL = "http://127.0.0.1:8000/api/waitlists/"
 
 export async function GET(request){
-    const authToken = getToken()
-    if(!authToken){
-        return NextResponse.json({}, {status: 401})
-    }
-
-    const options = {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-            "Accept": "application/json",
-            "Authorization": `Bearer ${authToken}`
-        }
-    }
-
-    const response = await fetch(DJANGO_API_WAITLISTS_URL, options)
-    console.log(response)
-
+    // const authToken = getToken()
+    // if(!authToken){
+    //     return NextResponse.json({}, {status: 401})
+    // }
+    const response = await ApiProxy.get(DJANGO_API_WAITLISTS_URL, true)
     const result = await response.json();
     let status = response.status
-    
-
-    return NextResponse.json({}, {status: status})
+    return NextResponse.json({...result}, {status: status})
 }
 
 export async function POST(request){
-    const requestData = await request.json()
-    const jsonData = JSON.stringify(requestData)
-    let headers = {
-        "Content-Type": "application/json",
-        "Accept": "application/json",
-        "Authorization": `Bearer ${authToken}`
-    }
-    const authToken = getToken()
-    if(authToken){
-        headers["Authorization"] = `Bearer ${authToken}`
-    }
-    const requestOptions = {
-        method: "POST",
-        headers: headers,
-        body: jsonData
-    }
-    const response = await fetch(DJANGO_API_WAITLISTS_URL, requestOptions)
+    const requestData = await request.json()   
+    const response = await ApiProxy.post(DJANGO_API_WAITLISTS_URL, requestData, true)
     try {
-        const responseData = await response.json()
+        await response.json()
     } catch (error) {
         NextResponse.json({message: "Invalid request"}, {status: response.status})
     }
