07b814a6bd5acbc7bfe295e343a5c051cad29923
andy <andyestevez27@gmail.com>
2024-09-20 13:09:51 -0400

    Updated apiproxy class

--- src/app/api/proxy.jsx
+++ src/app/api/proxy.jsx
@@ -13,6 +13,20 @@ export default class ApiProxy {
         return headers
     }
 
+    static async handleFetch(endpoint, requestOptions){
+        let data = {}
+        let status = 500
+        try {
+            const response = await fetch(endpoint, requestOptions)
+            data = await response.json()
+            status = response.status
+        } catch (error) {
+            data = {message: "Cannot reach API server", error: error}
+            status = 500
+        }
+        return {data, status}
+    }
+
     static async post(endpoint, object, requireAuth){ 
         const jsonData = JSON.stringify(object)
         const headers = await ApiProxy.getHeaders(requireAuth)
@@ -21,7 +35,7 @@ export default class ApiProxy {
             headers: headers,
             body: jsonData
         }
-        return await fetch(endpoint, requestOptions)
+        return await ApiProxy.handleFetch(endpoint, requestOptions)
     }
 
     static async get(endpoint, requireAuth){
@@ -30,6 +44,6 @@ export default class ApiProxy {
             method: "GET",
             headers: headers,
         }
-        return await fetch(endpoint, requestOptions)
+        return await ApiProxy.handleFetch(endpoint, requestOptions)
     }
 }
--- src/app/api/waitlists/route.jsx
+++ src/app/api/waitlists/route.jsx
@@ -9,23 +9,12 @@ export async function GET(request){
     // if(!authToken){
     //     return NextResponse.json({}, {status: 401})
     // }
-    const response = await ApiProxy.get(DJANGO_API_WAITLISTS_URL, true)
-    const result = await response.json();
-    let status = response.status
-    return NextResponse.json(result, {status: status})
+    const {data, status} = await ApiProxy.get(DJANGO_API_WAITLISTS_URL, true)
+    return NextResponse.json(data, {status: status})
 }
 
 export async function POST(request){
     const requestData = await request.json()   
-    const response = await ApiProxy.post(DJANGO_API_WAITLISTS_URL, requestData, true)
-    try {
-        await response.json()
-    } catch (error) {
-        NextResponse.json({message: "Invalid request"}, {status: response.status})
-    }
-    
-    if (response.ok){
-        return NextResponse.json({}, {status: 200})
-    }
-    return NextResponse.json({}, {status: 400})
+    const {data, status} = await ApiProxy.post(DJANGO_API_WAITLISTS_URL, requestData, true)
+    return NextResponse.json({data}, {status: status})
 }
